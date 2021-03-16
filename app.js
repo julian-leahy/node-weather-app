e57adec5445790079b891962fd3c1a7b6e210d38
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-16 20:16:42 +1300

    Get user input for location query

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const geocode = require('./utils/geocode.js');
 const forecast = require('./utils/forecast.js');
 
-geocode('Wellington New Zealand', (error, data) => {
-    if (error) {
-        return console.log(error);
-    }
-    forecast(data.latitude, data.longitude, (error, forecastData) => {
+const location = process.argv[2]
+
+if (location) {
+    geocode(location, (error, data) => {
         if (error) {
             return console.log(error);
         }
-        console.log(data.location)
-        console.log(forecastData);
-    })
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log(data.location)
+            console.log(forecastData);
+        })
 
-})
+    })
+} else {
+    console.log('Location required')
+}
