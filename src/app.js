7aeb1f9f7f965e167635378f19fa925814b33b1a
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-17 10:47:34 +1300

    Add handlebars templates and rename and set views directory

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
 const path = require('path');
 const express = require('express');
 
-const publicDir = path.join(__dirname, '../public');
-
 const app = express();
+
+// Define paths for Express config
+const publicDir = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDir));
 
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Home Page'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page'
+    });
+})
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'This is a message for the help page!'
+    });
+})
 
 app.get('/weather', (req, res) => {
     res.send({
