fb7ac8c0fbcd54866c3091168c8cc3c662249577
julian-leahy <julian.leahy.dev@gmail.com>
2021-03-16 20:09:02 +1300

    Handle errors

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const forecast = require('./utils/forecast.js');
 
 geocode('Wellington New Zealand', (error, data) => {
     if (error) {
-        console.log(error);
-    } else {
-        console.log(data.location)
-        forecast(data.latitude, data.longitude, (error, data) => {
-            console.log(error);
-            console.log(data);
-        })
+        return console.log(error);
     }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log(data.location)
+        console.log(forecastData);
+    })
+
 })
